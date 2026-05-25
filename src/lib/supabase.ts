554305c2845b0b42ca820c426bea088725bf527a
aguswijana <wijjanaagus453@gmai.com>
2026-05-25 10:19:24 +0800

    switch to supabase-js client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ucijquhbkxqycyajkjfa.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjaWpxdWhia3hxeWN5YWpramZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMzc5OTEsImV4cCI6MjA5NDYxMzk5MX0.kHBmx7t70dUFqXlT9980S80e9z8pcw-mGMnVSMxymN8i'
 
 export function createClient() {
-    return createBrowserClient(
-        'https://ucijquhbkxqycyajkjfa.supabase.co',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjaWpxdWhia3hxeWN5YWpramZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMzc5OTEsImV4cCI6MjA5NDYxMzk5MX0.kHBmx7t70dUFqXlT9980S80e9z8pcw-mGMnVSMxymN8i'
-    )
+    return createSupabaseClient(supabaseUrl, supabaseKey)
 }
