b64745451ed18472b109aba4e1300ec3fbfe9464
aguswijana <wijjanaagus453@gmai.com>
2026-05-26 08:45:56 +0800

    update publishable key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ucijquhbkxqycyajkjfa.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjaWpxdWhia3hxeWN5YWpramZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMzc5OTEsImV4cCI6MjA5NDYxMzk5MX0.kHBmx7t70dUFqXlT9980S80e9z8pcw-mGMnVSMxymN8i'
+const supabaseKey = 'sb_publishable_jYBG5ntQmyxdGHxy9YN2GQ_BLJr03gD'
 
 export function createClient() {
     return createSupabaseClient(supabaseUrl, supabaseKey)
